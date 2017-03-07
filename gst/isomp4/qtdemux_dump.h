1485e551862847349a60cc53d6e7ae69539ce86a
Rémi Cohen-Scali <remi.cohen-scali@nagra.com>
2017-03-07 13:52:35 +0100

    Merge problem with qtdemux_dump_svmi decl

--- gst/isomp4/qtdemux_dump.h
+++ gst/isomp4/qtdemux_dump.h
@@ -83,8 +83,6 @@ gboolean qtdemux_dump_sdtp (GstQTDemux * qtdemux, GstByteReader * data,
     int depth);
 gboolean qtdemux_dump_tfdt (GstQTDemux * qtdemux, GstByteReader * data,
     int depth);
-gboolean qtdemux_dump_svmi (GstQTDemux * qtdemux, GstByteReader * data,
-    int depth);
 gboolean qtdemux_dump_senc (GstQTDemux * qtdemux, GstByteReader * data,
     int depth);
 gboolean qtdemux_dump_unknown (GstQTDemux * qtdemux, GstByteReader * data,
